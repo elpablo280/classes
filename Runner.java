b1ca785ee96a2a81324dd6a7070d846cc5532475
elpablo280 <65726489+elpablo280@users.noreply.github.com>
2021-11-21 20:58:54 +0300

    Add files via upload

--- Runner.java
+++ Runner.java
@@ -5,15 +5,15 @@ public class Runner {
     public static void main(String[] args) {
 
         // create the steel wire
-        Material material1 = new Material("steel");
+        Material material1 = new Material("steel", 7850.0);
         Subject steelWire = new Subject("wire", material1, 0.03);
 
         // output the object content
         System.out.println(steelWire);
 
         // update the wire material and output it's mass
-        Material material2 = new Material("copper");
+        Material material2 = new Material("copper", 8500.0);
         Subject copperWire = new Subject("wire", material2, 0.03);
-        System.out.println("The wire mass is " + copperWire.getMass());
+        System.out.println("The wire mass is " + copperWire.getMass() + " kg");
     }
 }
